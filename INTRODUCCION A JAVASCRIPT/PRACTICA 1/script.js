cfe897a1f8fbc9f25c88382b781d2c8b972015b1
Andres Alarcon <aalarcon@transfiriendo.com>
2024-11-13 00:20:23 -0500

    Introducción a JavaScript

---
+++ INTRODUCCION A JAVASCRIPT/PRACTICA 1/script.js
@@ -0,0 +1,24 @@
+let name = "Andres"
+let isMale = true
+let age = 27
+const birthDay = "Feb 7"
+const pets = [
+    {
+        Name: "Tito",
+        Type: "Cat"
+    },
+    {
+        Name: "Eevee",
+        Type: "Cat"
+    }
+]
+
+const person = {
+    name, 
+    isMale,
+    age, 
+    birthDay,
+    pets
+}
+
+console.log(person)
